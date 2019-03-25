bb895443d1e18802ec388444c5beb5f0752ad125
Iriabow <guilderwars@gmail.com>
2019-03-25 14:59:18 +0100

    Cambio en server.js

--- server.js
+++ server.js
@@ -13,8 +13,6 @@ https.createServer({
   key: fs.readFileSync('server.key'),
   cert: fs.readFileSync('server.cert')
 }, app)
-.listen(3000, function () {
+.listen(process.env.PORT || 5000, function () {
   console.log('Example app listening on port 3000! Go to https://localhost:3000/')
 })
-
-
