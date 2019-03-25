02c3b0880b8468f1c4e1bce69b7c9aa530c035ea
Iriabow <guilderwars@gmail.com>
2019-03-25 17:27:53 +0100

    Paquete para https

--- server.js
+++ server.js
@@ -1,20 +1,15 @@
 const express = require('express');
 const path = require('path');
 const serveStatic = require('serve-static');
-
+var sslRedirect = require('heroku-ssl-redirect'
 let app = express();
-app.use(serveStatic(__dirname + "/dist"));
+app.use(sslRedirect(), serveStatic(__dirname + "/dist"));
 
+app.get('/', function(req, res){
+  res.send('hello world');
+});
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log('Listening on port ' + port)
 });
 
-if(process.env.NODE_ENV === 'production') {
-  app.use((req, res, next) => {
-    if (req.header('x-forwarded-proto') !== 'https')
-      res.redirect(`https://${req.header('host')}${req.url}`)
-    else
-      next()
-  })
-}
