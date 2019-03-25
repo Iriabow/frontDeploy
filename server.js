dccb56be562d94c2e51401b5ca946094e4b93fb8
Iriabow <guilderwars@gmail.com>
2019-03-25 17:18:07 +0100

    Restriccion https

--- server.js
+++ server.js
@@ -10,3 +10,11 @@ app.listen(port, () => {
   console.log('Listening on port ' + port)
 });
 
+if(process.env.NODE_ENV === 'production') {
+  app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https')
+      res.redirect(`https://${req.header('host')}${req.url}`)
+    else
+      next()
+  })
+}
